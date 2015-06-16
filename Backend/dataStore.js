5522d4d5e2471361885b65653ecaafcb13f046b6
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2015-06-17 02:07:54 +0300

    Add solution to source control

---
+++ Backend/dataStore.js
@@ -0,0 +1,35 @@
+﻿function User(id, username) {
+    this.id = id;
+    this.username = username;
+}
+
+function DataProvider() {
+    
+    var pg = require('pg'),
+        connectionString = 'postgres://postgres:' + encodeURIComponent('Password#1') + '@localhost:5432/NodePoc',
+        client = new pg.Client(connectionString),
+        _ = require('lodash');
+    
+    this.users = function (callback) {
+        
+        client.connect(function (err) {
+            if (err) {
+                callback(err, null); return;
+            }
+            client.query('SELECT "Id","Username" FROM "Users"', function (err, result) {
+                if (err) {
+                    callback(err, null); return;
+                }
+                
+                client.end();
+                
+                callback(null, _.map(result.rows, function (row) {
+                    return new User(row.Id, row.Username);
+                }));
+            });
+        });
+        
+    };
+}
+
+module.exports = new DataProvider();
---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+﻿var http = require('http'),
+    data = require('./dataStore'),
+    port = process.env.port || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    data.users(function (err, users) {
+        if (err) {
+            res.write(JSON.stringify({ Error: err }), 'utf8');
+            res.end();
+            return;
+        }
+        res.write(JSON.stringify(users), 'utf8');
+        res.end();
+    });
+}).listen(port);
